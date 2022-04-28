dd47e8bea0a20b32d8a4ca5431511b024a4574ac
Mariusz PS <k8kpol@gmail.com>
2022-04-28 12:04:25 +0200

    Rechnenart

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,18 +4,44 @@ public class App {
     
     public static void main(String[] args) {
      
-    //    ausgabe("Ergebnis +: " + addieren(2, -1));
-    //    ausgabe("Ergebnis -: " + subtrahieren(5, 4));
-    //    ausgabe("Ergebnis *: " + multiplizieren(5, 4));
-       
-       ausgabe("Ergebnis /: " + dividieren(5, 2));
-       ausgabe("Ergebnis /: " + dividieren(0, 5));
-       ausgabe("Ergebnis /: " + dividieren(5, 0));
+        ausgabe("----------  Tests --------"); 
+        ausgabe("Ergebnis +: " + addieren(2, -1));
+        ausgabe("Ergebnis -: " + subtrahieren(5, 4));
+        ausgabe("Ergebnis *: " + multiplizieren(5, 4));
+        
+        ausgabe("Ergebnis /: " + dividieren(5, 2));
+        ausgabe("Ergebnis /: " + dividieren(0, 5));
+        ausgabe("Ergebnis /: " + dividieren(5, 0));
+
+        ausgabe("---------  Rechner ----------");
+        ausgabe(rechner(4,5,"+"));
+        ausgabe(rechner(4,5,"-"));
+        ausgabe(rechner(4,5,"*"));
+        ausgabe(rechner(4,5,":"));
+        ausgabe(rechner(4,0,":"));
+        ausgabe(rechner(4,5,"#!?:"));
 
     }
 
     // Rechenart
     // +/-/*/:  ???
+
+    // Rechner
+    public static String rechner(double a, double b, String op) {
+        switch (op) {
+            case "+":
+                return "Ergebnis: " + addieren(a,b);
+            case "-":
+                return "Ergebnis: " + subtrahieren(a,b);
+            case "*":
+                return "Ergebnis: " + multiplizieren(a,b);
+            case ":":
+                return "Ergebnis: " + dividieren(a,b);
+            default:
+                return "Irgendwas ging schief!";
+        }
+    }
+
 
     // Division
     public static String dividieren(double a, double b) {
@@ -28,20 +54,21 @@ public class App {
        
     }
 
-    // Addition OK!
-    public static double addieren(double a, double b) {
-        return a + b;
+     // Multiplikation OK
+     public static double multiplizieren(double a, double b) {
+        return a * b;
     }
-
+    
     // Substraktion OK
     public static double subtrahieren(double a, double b) {
         return a - b;
     }
 
-    // Multiplikation OK
-    public static double multiplizieren(double a, double b) {
-        return a * b;
+     // Addition OK!
+     public static double addieren(double a, double b) {
+        return a + b;
     }
+   
 
    
     // Ausgabe
